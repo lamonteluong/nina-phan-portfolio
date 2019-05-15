7099e32cc1fc4fc4fbf5cfd96239d5235f4c886a
lluong <lamonte.luong@gmail.com>
2019-05-15 01:18:14 -0400

    basic dark theme added, not complete

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 let mainNav = document.querySelector('.main-nav');
-
 let navbarToggle = document.querySelector('.navbar-toggle');
 
+let themeToggle = document.querySelector('.theme-toggle');
+let navLinks = document.querySelectorAll('.nav-link');
+
+/* FIX: Class added, display: block overriden by display: none */ 
 navbarToggle.addEventListener('click', () => {
     mainNav.classList.toggle('show');
-
     console.log(mainNav);
+});
+
+
+/* FIX: Class added, color: white overriden by color: black */ 
+themeToggle.addEventListener('click', () => {
+    document.querySelector('body').classList.toggle('dark-theme');
+    navLinks.forEach((link) => {
+        link.classList.toggle('dark-theme');
+        console.log(link); 
+    });
 });
