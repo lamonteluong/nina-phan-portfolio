42bb0a85994eb78ccb93aaf2aef29f66036f07ab
lluong <lamonte.luong@gmail.com>
2019-05-22 00:36:15 -0400

    adjusted fonts & margins; added scroll-to-top & contact links

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let mainNav = document.querySelector('.main-nav');
 let navbarToggle = document.querySelector('.navbar-toggle');
 let navLinks = document.querySelectorAll('.nav-link');
+let dropDown = false;
 
 let themeToggle = document.querySelector('.theme-toggle');
 let darkMode = false;
@@ -8,9 +9,21 @@ let darkMode = false;
 let viewWork = document.querySelector('.view-work-container');
 let arrowDown = document.getElementById('arrow-down');
 
+let backTopBtn = document.getElementById('back-top-btn');
+let backTop1 = document.getElementById('back-top-1');
+let backTop2 = document.getElementById('back-top-2');
+
 /* FIXME: Class added, display: block overriden by display: none */ 
 navbarToggle.addEventListener('click', () => {
-    mainNav.classList.toggle('show');
+    //mainNav.classList.toggle('show');
+    /* FIXME: Desktop resize to mobile & back => links disappear */
+    if (dropDown == false) {
+        mainNav.style.display = 'block';
+        dropDown = true;
+    } else {
+        mainNav.style.display = 'none';
+        dropDown = false;
+    }
 });
 
 /* Toggle for Light/Dark Mode */
@@ -19,9 +32,13 @@ themeToggle.addEventListener('click', () => {
     if (darkMode == false) {
         darkMode = true;
         arrowDown.style.fill = 'white';
+        backTop1.style.stroke = 'white';
+        backTop2.style.fill = 'white';
     } else {
         darkMode = false;
         arrowDown.style.fill = 'black';
+        backTop1.style.stroke = 'black';
+        backTop2.style.fill = 'black';
     }
 
     navLinks.forEach((link) => {
@@ -33,7 +50,7 @@ themeToggle.addEventListener('click', () => {
     });
 });
 
-/* Change navbar link colour based on the current theme */
+/* Change navbar link colour:hover based on the current theme */
 navLinks.forEach((link) => {
     link.addEventListener('mouseenter', () => {
         link.style.color = '#33B8A8';
@@ -51,4 +68,17 @@ navLinks.forEach((link) => {
 /* TODO: animate to make smooth */
 viewWork.addEventListener('click', () => {
     document.querySelector('.project').scrollIntoView();
+});
+
+/* Button to scroll back to top */
+window.onscroll = () => {
+    if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
+        backTopBtn.style.display = "block";
+    } else {
+        backTopBtn.style.display = "none";
+    }
+};
+backTopBtn.addEventListener('click', () => {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE & Opera
 });
